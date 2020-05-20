d00581ea269437558bf2e98d1e5ab98b7e9e42d7
David Lyu <lyu.david@yahoo.com>
2020-05-19 17:13:37 -0700

    stuff

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const {JSDOM} = require("jsdom");
 const {window} = new JSDOM("");
 const $ = require("jquery")(window)
 require('dotenv/config')
+const ClientError = require('./client-error');
 
 app.get("/api/task/:projectId",(req,res,next)=>{
   const projectId = req.params.projectId;
@@ -20,6 +21,33 @@ app.get("/api/task/:projectId",(req,res,next)=>{
   })
 })
 
+app.use(express.json())
+app.post("/api/task", (req,res,next)=>{
+  const {content, projectId } = req.body
+  let dueDate = req.body.due
+  if(!dueDate){
+    dueDate = "today"
+  }
+  console.log(req.body)
+  $.ajax(
+  {
+    url: "https://api.todoist.com/rest/v1/tasks",
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${process.env.TODOIST}`,
+      "Content-Type": "application/json"
+    },
+    data: JSON.stringify({
+      "content": content,
+      "due_string": dueDate,
+      project_id: projectId
+    }),
+    dataType: "json",
+    success: console.log,
+    error: console.error
+  })
+})
+
 app.get("/api/recommendation/:query",(req,res,next)=>{
   const queryKey = req.params.query;  // getRecommendation(queryKey) {
     $.ajax(
@@ -28,10 +56,11 @@ app.get("/api/recommendation/:query",(req,res,next)=>{
         method: "GET",
         data: {
           "q": queryKey,
-          "k": process.env.TASTEDIVE
+          "k": process.env.TASTEDIVE,
+          limit: 5
         },
-        success: console.log,
-        error: console.error
+        success: (data)=> res.status(200).json(data),
+        error: err=> next(err)
       }
     )
 })
