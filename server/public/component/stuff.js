103fe92abe4181b42b7bab8cb10b56a0bd3f9deb
David Lyu <lyu.david@yahoo.com>
2020-05-28 16:19:19 -0700

    making a background code look

---
+++ server/public/component/stuff.js
@@ -0,0 +1,18 @@
+class Welcome {
+  constructor(){
+    const TO = What todo?
+  }
+
+  keepOrganized() {
+    const todos = [proj1,proj2,proj3]
+    for (let index = 0; index < todos.length; i++){
+      console.log(todos[index])
+    }
+  }
+
+  doStuff(today) {
+    const whatToDo = "I don't know you tell me"
+  }
+
+
+}
